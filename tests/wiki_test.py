25ea5236eef5c09ff08d9bcc2e42eafcbe242384
AleksSh <Coluchy@yandex.ru>
2025-03-29 19:03:59 +0600

    fixes added

--- tests/wiki_test.py
+++ tests/wiki_test.py
@@ -11,14 +11,17 @@ def test_skip_pages():
     with allure.step('Проверка первой страницы'):
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(
             have.text('The Free Encyclopedia\n…in over 300 languages'))
+
     with allure.step('Проверка второй страницы'):
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(
             have.text('New ways to explore'))
+
     with allure.step('Проверка третьей страницы'):
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(
             have.text('Reading lists with sync'))
+
     with allure.step('Проверка четвёртой страницы'):
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(have.text('Data & Privacy'))
@@ -38,6 +41,9 @@ def test_search_wiki_appium():
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
+
+    with step('Кликаем элемент'):
+        browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).first.click()
 
 
 @allure.story('Search')
